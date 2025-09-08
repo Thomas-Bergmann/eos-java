391d473fda354b3c5418aecfb492d1cad588a4a6
Thomas Bergmann <tm@hatoka.de>
2025-09-08 13:34:09 +0200

    fix: remove unused dependencies (#27)
    * update quarkus

--- devices/build.gradle.kts
+++ devices/build.gradle.kts
@@ -8,28 +8,19 @@ dependencies {
     implementation("org.slf4j:slf4j-api")
     implementation("org.apache.commons:commons-csv")
 
-    implementation("jakarta.enterprise:jakarta.enterprise.cdi-api")
     implementation("jakarta.inject:jakarta.inject-api")
-    implementation("jakarta.ws.rs:jakarta.ws.rs-api")
     implementation("jakarta.validation:jakarta.validation-api")
-    implementation("jakarta.annotation:jakarta.annotation-api")
-    implementation("jakarta.xml.bind:jakarta.xml.bind-api")
 
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
 
     implementation("io.quarkus:quarkus-arc")
-    implementation("io.quarkus:quarkus-scheduler")
 
-    // Prometheus metrics
-    implementation("io.quarkus:quarkus-micrometer-registry-prometheus")
-    
     // InfluxDB client for Grafana visualization
-    implementation("com.influxdb:influxdb-client-java:6.10.0")
+    implementation("com.influxdb:influxdb-client-java")
 
     testImplementation("io.quarkus:quarkus-junit5")
-    testImplementation("io.rest-assured:rest-assured")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
