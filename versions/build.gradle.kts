5c6f2424958e65c6efa18b86ed23414e4da713cd
Thomas Bergmann <tm@hatoka.de>
2025-09-08 13:30:06 +0200

    fix: remove unused dependencies * update quarkus

--- versions/build.gradle.kts
+++ versions/build.gradle.kts
@@ -3,16 +3,15 @@ plugins {
 }
 
 // Define dependency versions for dependency groups
-val fasterXMLVersion = "2.20"
+val fasterXMLVersion = "2.20.0"
 
 javaPlatform.allowDependencies()
 
 dependencies {
-    api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.24.2"))
+    api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.26.2"))
 
     constraints {
 
-        // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
         api("org.apache.commons:commons-lang3:3.18.0")
         api("org.apache.commons:commons-csv:1.14.1")
 
@@ -20,14 +19,9 @@ dependencies {
         api("com.fasterxml.jackson.core:jackson-databind:${fasterXMLVersion}")
         api("com.fasterxml.jackson.core:jackson-annotations:${fasterXMLVersion}")
 
-        api("com.microsoft.azure:msal4j:1.21.0")
-        api("com.azure:azure-identity:1.16.2")
-        api("com.azure:azure-storage-blob:12.30.1")
-        api("com.azure:azure-data-tables:12.5.4")
-        api("com.azure:azure-storage-file-share:12.27.2")
+        // time/metrics export
+        api("com.influxdb:influxdb-client-java:6.10.0")
 
-        // Prometheus metrics
-        api("io.micrometer:micrometer-registry-prometheus:1.14.3")
     }
 }
 
